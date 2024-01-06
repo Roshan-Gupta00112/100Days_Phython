ac3299d97b9f2857ebca64c73da72702f6ba1b08
Roshan Kumar <roshangupta00112@gmail.com>
2024-01-06 12:11:26 +0530

    Day1

---
+++ Day1/sheet1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+city = input("what's name of the City You grew up in?\n")
+
+pet = input("What's your pet's name?\n")
+
+print(f"Your Band Name could be {city} {pet}")
---
+++ Day1/sheet2.py
@@ -0,0 +1,8 @@
+print("Good Morning!"*3, end="\t")
+
+print("Good Morning!\n"*3)
+
+name = input("What is your name?\n")
+print(len(name))
+
+print(len(input("What is your name?\n")))
