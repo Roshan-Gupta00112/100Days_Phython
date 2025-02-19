a8da283470339525860e2f78c2c9145908c330cb
Roshan Kumar <roshangupta00112@gmail.com>
2025-02-19 13:10:13 +0530

    Add files via upload

---
+++ Day9/key_not_exist_in_dict.py
@@ -0,0 +1,25 @@
+# Library     Method for Data Type        Specification
+# Pyhton      type()                      Function
+# NumPy       .dtype                      Attribute
+# Pandas      .dtype                      Attribute
+# PySpark     .dtypes                     Attribute
+
+
+my_dict = {
+    "a": 23,
+    "b": 'f',
+    "aim": "billionaire",
+    "2": 3600000,
+    "4": "5000000",
+    "5.5": "7000000",
+    "7": 8000000.5
+}
+
+
+
+
+# for key, val in my_dict.items():
+#     print(f"{key} {type(key)}: {val} {type(val)}")
+
+
+print(my_dict[0])
